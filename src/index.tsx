6d82e651d6d75237ceea21b422b7fe0bd8c30920
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-01 15:43:12 -0500

    removed some unneeded files

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Tracker from './containers/Tracker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Tracker />
   </React.StrictMode>,
   document.getElementById('root')
 );
