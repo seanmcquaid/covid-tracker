ae933150f68ee397c286e7b60d55564437bd4bd1
seanmcquaid <sean.mcquaid2@verizon.net>
2021-01-03 10:18:07 -0500

    will need to refactor some naming conventions but the mode changing works

--- src/containers/Tracker/Chart.tsx
+++ src/containers/Tracker/Chart.tsx
@@ -1,84 +1,91 @@
-import { ResponsiveLine } from '@nivo/line';
-import { memo } from 'react';
+import { ResponsiveBar } from '@nivo/bar';
+import { memo, useCallback, useMemo, useState } from 'react';
+import Button from '../../components/Button';
 
 type ChartProps = {
   data: any[];
 };
 
 const Chart: React.FC<ChartProps> = memo(({ data }) => {
-  const pastWeekData = data.slice(0, 7);
-  console.log(pastWeekData);
-  const moddedData = pastWeekData.map((dateInfo) => {
-    return {
-      id: dateInfo.dateModified,
-      color: '#000',
-      data: [
-        { x: 'Deaths', y: dateInfo.death },
-        { x: 'Positive Cases Increase', y: dateInfo.positiveIncrease },
-        { x: 'Total Cases', y: dateInfo.total },
-      ],
-    };
-  });
+  const pastWeekData = data.slice(0, 7).reverse();
+  const modes = useMemo(() => ['positive', 'deathIncrease', 'total'], []);
+  const [currentMode, setCurrentMode] = useState('');
+
+  const selectModeOnClick = useCallback((mode) => {
+    setCurrentMode(mode);
+  }, []);
+
   return (
     <div style={{ height: '500px' }}>
-      <ResponsiveLine
-        data={moddedData}
-        margin={{ top: 50, right: 110, bottom: 50, left: 60 }}
-        xScale={{ type: 'point' }}
-        yScale={{ type: 'linear', min: 'auto', max: 'auto', stacked: true, reverse: false }}
-        yFormat=" >-.2f"
+      <div>
+        {modes.map((modeName, i) => (
+          <Button type="button" onClick={() => selectModeOnClick(modeName)}>
+            {modeName}
+          </Button>
+        ))}
+      </div>
+      <ResponsiveBar
+        data={pastWeekData}
+        keys={[currentMode.length > 0 ? currentMode : 'total']}
+        indexBy="date"
+        margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+        padding={0.3}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        colors={{ scheme: 'nivo' }}
+        defs={[
+          {
+            id: 'dots',
+            type: 'patternDots',
+            background: 'inherit',
+            color: '#38bcb2',
+            size: 4,
+            padding: 1,
+            stagger: true,
+          },
+          {
+            id: 'lines',
+            type: 'patternLines',
+            background: 'inherit',
+            color: '#eed312',
+            rotation: -45,
+            lineWidth: 6,
+            spacing: 10,
+          },
+        ]}
+        borderColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
         axisTop={null}
         axisRight={null}
-        axisBottom={{
-          orient: 'bottom',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'transportation',
-          legendOffset: 36,
-          legendPosition: 'middle',
-        }}
-        axisLeft={{
-          orient: 'left',
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: 'count',
-          legendOffset: -40,
-          legendPosition: 'middle',
-        }}
-        pointSize={10}
-        pointColor={{ theme: 'background' }}
-        pointBorderWidth={2}
-        pointBorderColor={{ from: 'serieColor' }}
-        pointLabelYOffset={-12}
-        useMesh={true}
+        labelSkipWidth={12}
+        labelSkipHeight={12}
+        labelTextColor={{ from: 'color', modifiers: [['darker', 1.6]] }}
         legends={[
           {
+            dataFrom: 'keys',
             anchor: 'bottom-right',
             direction: 'column',
             justify: false,
-            translateX: 100,
+            translateX: 120,
             translateY: 0,
-            itemsSpacing: 0,
-            itemDirection: 'left-to-right',
-            itemWidth: 80,
+            itemsSpacing: 2,
+            itemWidth: 100,
             itemHeight: 20,
-            itemOpacity: 0.75,
-            symbolSize: 12,
-            symbolShape: 'circle',
-            symbolBorderColor: 'rgba(0, 0, 0, .5)',
+            itemDirection: 'left-to-right',
+            itemOpacity: 0.85,
+            symbolSize: 20,
             effects: [
               {
                 on: 'hover',
                 style: {
-                  itemBackground: 'rgba(0, 0, 0, .03)',
                   itemOpacity: 1,
                 },
               },
             ],
           },
         ]}
+        animate={true}
+        motionStiffness={90}
+        motionDamping={15}
       />
     </div>
   );
